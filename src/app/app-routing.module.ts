74d923cac4ad82382219da1cf6351969e544dd0a
Benjamin <benjaschell@gmail.com>
2023-04-13 22:15:33 -0300

    firstCommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { BienvenidoComponent } from './bienvenido/bienvenido.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BienvenidoComponent},
+  {path: 'bienvenido', component: BienvenidoComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'error', component: ErrorComponent},
+  {path: '**', component: ErrorComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
